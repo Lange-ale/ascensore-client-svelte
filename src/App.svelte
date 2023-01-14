65add8144e3c5fc65319f4f3e152b52b0fec72d2
unknown <alessiolangellotti2005@gmail.com>
2023-01-14 11:36:50 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  let piano = 0;
+  let direzione = 0; //-1 = scendere, 0 = fermo, 1 = salire;
+</script>
+
+<main>
+  <h1 class="text-center text-4xl font-bold">
+    Piano dell'ascensore:
+  </h1>
+  <div class="grid grid-flow-col gap-5 text-center auto-cols-max justify-center">
+    <div class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+      <span class="countdown font-mono text-5xl">
+        <span style="--value:{piano};"></span>
+      </span>
+    </div>
+  </div>
+</main>
