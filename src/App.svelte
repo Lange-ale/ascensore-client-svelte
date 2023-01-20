d1ec2dc6505fb42221d8493ecce1dfdbd7c23580
Alessio Langellotti <alessiolangellotti2005@gmail.com>
2023-01-20 13:53:08 +0100

    fetch to update the state + UI

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,56 @@
 <script>
-  let piano = 0;
-  let direzione = 0; //-1 = scendere, 0 = fermo, 1 = salire;
+import { state } from './store.js';  
+  
+function getData(){
+    fetch("https://middlewaredb.langellotti.repl.co/get_data")
+      .then((response) => response.json())
+      .then((json) => {
+        console.log($state.piano)
+        //TODO: aggiornare lo stato
+      })
+    setTimeout(getData, 1000);
+  }; 
+  getData()
 </script>
 
 <main>
-  <h1 class="text-center text-4xl font-bold">
+  <br>
+  <h1 class="text-center text-6xl font-bold" style="color: lightgreen;">
     Piano dell'ascensore:
   </h1>
+  <br>
+
   <div class="grid grid-flow-col gap-5 text-center auto-cols-max justify-center">
     <div class="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
-      <span class="countdown font-mono text-5xl">
-        <span style="--value:{piano};"></span>
+      <span class="countdown font-mono text-8xl">
+        <span style="--value:{$state.piano};"></span>
       </span>
     </div>
   </div>
+  
+  <div class="flex w-full">
+    <div class="grid h-100 flex-grow card place-items-center">
+      <img src="../img/ascensore.jpg" alt="ascensore" style="width: 30%;">
+    </div>
+    <div class="grid h-100 flex-grow card place-items-center">
+      <div style="text-align: center;">
+        <label class="swap text-8xl">
+          {#if $state.direzione == 1}
+            <div class="swap-on" style="background-color: green;">⤊</div>
+          {:else}
+            <div class="swap-off">⤊</div>
+          {/if}
+        </label>  
+      </div>
+      <br>
+      <div style="text-align: center;">
+        <label class="swap text-8xl">
+          {#if $state.direzione == -1}
+            <div class="swap-on" style="background-color: red;">⤋</div>
+          {:else}
+            <div class="swap-off">⤋</div>
+          {/if}
+        </label>
+      </div>
+    </div>
 </main>
